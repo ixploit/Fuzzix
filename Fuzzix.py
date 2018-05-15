e967d61705600b510d069da912c0a801cb92dc0b
AnonymOnline <david@anonym-online.net>
2018-05-15 21:46:42 +0200

    reformatting code

--- Fuzzix.py
+++ Fuzzix.py
@@ -16,16 +16,23 @@ class URL_Fuzzer:
 
         Logger.info("fuzzing url", self.host.getURL())
 
-
     def __init_wordlists__(self):
         """
         reads the path of the wordlists out of the Settings and reads the wordlists into memory
         return: None
         """
-        self.DirectoriesWordlist = Wordlist("directories", Settings.readAttribute("WORDLISTS/directories",""))
-        self.ExtensionsWordlist  = Wordlist("extension", Settings.readAttribute("WORDLISTS/extensions",""))
-        self.FilesWordlist       = Wordlist("files", Settings.readAttribute("WORDLISTS/files",""))
-        self.mutationsWordlist   = Wordlist("mutations", Settings.readAttribute("WORDLISTS/mutations",""))
+        self.directories_wordlist = Wordlist("directories",
+                                             Settings.readAttribute(
+                                                 "WORDLISTS/directories", ""))
+        self.ExtensionsWordlist = Wordlist("extension",
+                                           Settings.readAttribute(
+                                               "WORDLISTS/extensions", ""))
+        self.FilesWordlist = Wordlist("files",
+                                      Settings.readAttribute(
+                                          "WORDLISTS/files", ""))
+        self.mutationsWordlist = Wordlist("mutations",
+                                          Settings.readAttribute(
+                                              "WORDLISTS/mutations", ""))
 
     @staticmethod
     def __spiderworker__(content):
@@ -34,19 +41,20 @@ class URL_Fuzzer:
         attribute content: the content to proceed
         return: A proceeded content object
         """
-        rootURL = content.getURL() 
+        rootURL = content.getURL()
         newContent = Content(rootURL)
         newContent.setContentType(content.getContentType())
         newContent.setStatus(content.getStatus())
-        
+
         if content.getStatus() in URL.GOOD_STATUS:
-            if ('text' in content.getContentType() or 'script' in content.getContentType()):
+            if ('text' in content.getContentType()
+                    or 'script' in content.getContentType()):
                 refs = WebApi.grabRefs(content.getContent())
-                urls =[]
+                urls = []
                 for ref in refs:
                     try:
                         #try to prettify url
-                        url = URL.prettifyURL(rootURL,ref)
+                        url = URL.prettifyURL(rootURL, ref)
                         urls.append(url)
                     except ValueError:
                         continue
@@ -56,44 +64,45 @@ class URL_Fuzzer:
                 newContent.setContent(urls)
         return newContent
 
-
     def spider(self):
         """
         spider-routine of the URL_Fuzzer
         return: None
         """
         Logger.info("Spidering URL", self.host.getURL())
-        toProceed=[] #buffer for open tasks
-        doneURLs=[] #deadlock protection
+        toProceed = []  #buffer for open tasks
+        doneURLs = []  #deadlock protection
 
         #starting on website-root
         rootcontent = Content(self.host.getURL())
         rootcontent.setProcessor(URL_Fuzzer.__spiderworker__)
         toProceed.append(rootcontent)
 
-        for i in range(0, Settings.readAttribute("recursion_depth",0)):
-            
+        for i in range(0, Settings.readAttribute("recursion_depth", 0)):
+
             #writing buffer in queue
-            for a in range(0,len(toProceed)):
+            for a in range(0, len(toProceed)):
                 content = toProceed.pop()
                 Content_Worker.queue.put(content)
-            
+
             #waiting for workers to finish
-            Logger.info('Processing recursion', i, Content_Worker.queue.qsize(), 'task(s) to be done')
+            Logger.info('Processing recursion', i,
+                        Content_Worker.queue.qsize(), 'task(s) to be done')
             Content_Worker.queue.join()
-            
+
             #processing finished resulsts
             while not Content_Worker.done.empty():
                 content = Content_Worker.done.get()
                 Content_Worker.done.task_done()
-                
+
                 if content.getContentType() != "linklist":
                     continue
-                    
+
                 urls = content.getContent()
                 for url in urls:
                     path = url.getPath()
-                    if self.host.isExternal(url) or url.getURL() in doneURLs or len(path) == 0:
+                    if self.host.isExternal(
+                            url) or url.getURL() in doneURLs or len(path) == 0:
                         continue
                     doneURLs.append(url.getURL())
                     length = content.getSize()
@@ -102,7 +111,7 @@ class URL_Fuzzer:
                     newContent.setProcessor(URL_Fuzzer.__spiderworker__)
                     toProceed.append(newContent)
 
-        #printing result    
+        #printing result
         print(self.host.getRootdir())
         Logger.info("spidering completed")
 
@@ -120,7 +129,7 @@ class URL_Fuzzer:
         attribute dir: the root dir for the scan
         return: the updated dir
         """
-    
+
     def __scan_for_files__(dir):
         """
         scans for hidden files in the given dir and appends them to the dir
@@ -134,23 +143,21 @@ class URL_Fuzzer:
         attribute dir: the root dir for the scan
         return: the updated dir
         """
-    
+
     def __scan_dir__(dir):
         """
         runs all scans over a given dir recursively and appends any hits to the root-dir
         attribute dir: the root dir for the scan
         return: the updated dir
         """
-        
+
     def fuzz(self):
         Logger.info("fuzzing URL", self.host.getURL())
-        recursion_depth = Settings.readAttribute("recursion_depth",0)
+        recursion_depth = Settings.readAttribute("recursion_depth", 0)
         for i in range(0, recursion_depth):
-            Logger.info("Processing recursion",i)
+            Logger.info("Processing recursion", i)
 
-        
         Logger.info("fuzzing completed")
-
 
 
 def startup():
@@ -161,13 +168,31 @@ def startup():
 
     # parsing command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('url', metavar='url', help="the victims adress", type=str, nargs='+')
-    parser.add_argument('-t', '--threads', help="the amount of threads to use", type=int, default=64)
-    parser.add_argument('-r', '--recursion', help="the maximum recursion depth", type=int, default=5)
-    parser.add_argument("-s", "--spider", action="store_true",
-                        help="spider the given website")
-    parser.add_argument("-f", "--fuzz", action="store_true", help="fuzz the given url to discover hidden files")
-    parser.add_argument("--verifyCert", action="store_true", help="verify the hosts certificate")
+    parser.add_argument(
+        'url', metavar='url', help="the victims adress", type=str, nargs='+')
+    parser.add_argument(
+        '-t',
+        '--threads',
+        help="the amount of threads to use",
+        type=int,
+        default=64)
+    parser.add_argument(
+        '-r',
+        '--recursion',
+        help="the maximum recursion depth",
+        type=int,
+        default=5)
+    parser.add_argument(
+        "-s", "--spider", action="store_true", help="spider the given website")
+    parser.add_argument(
+        "-f",
+        "--fuzz",
+        action="store_true",
+        help="fuzz the given url to discover hidden files")
+    parser.add_argument(
+        "--verifyCert",
+        action="store_true",
+        help="verify the hosts certificate")
 
     opts = parser.parse_args()
 
@@ -185,17 +210,18 @@ def startup():
         if not URL.isValidURL(host_url):
             raise ValueError(host_url + " is not a valid URL")
 
-        Settings.writeAttribute("host_url",host_url)
-        Settings.writeAttribute("spider",spider)
-        Settings.writeAttribute("fuzz",fuzz)
-        Settings.writeAttribute("verify_cert",verify_cert)
-        Settings.writeAttribute("threads",threads)
-        Settings.writeAttribute("recursion_depth",recursion_depth)
+        Settings.writeAttribute("host_url", host_url)
+        Settings.writeAttribute("spider", spider)
+        Settings.writeAttribute("fuzz", fuzz)
+        Settings.writeAttribute("verify_cert", verify_cert)
+        Settings.writeAttribute("threads", threads)
+        Settings.writeAttribute("recursion_depth", recursion_depth)
 
         #read config-file
         Settings.readConfig("config/config.ini")
 
-        WebApi.setProtocol(URL(Settings.readAttribute("host_url","")).getProto())
+        WebApi.setProtocol(
+            URL(Settings.readAttribute("host_url", "")).getProto())
     except ValueError as e:
         raise e
 
@@ -250,22 +276,21 @@ if __name__ == "__main__":
 
     try:
         startup()
-        startWorkers(Settings.readAttribute("threads",0))
+        startWorkers(Settings.readAttribute("threads", 0))
     except ValueError as e:
         Logger.error(e)
         exit()
 
     targetHost = Host(
-        URL(Settings.readAttribute("host_url","")), 
-        Dir(Settings.readAttribute("root_dir",""), None)
-        )
+        URL(Settings.readAttribute("host_url", "")),
+        Dir(Settings.readAttribute("root_dir", ""), None))
 
     urlFuzzer = URL_Fuzzer(targetHost)
 
-    if Settings.readAttribute("spider",False):
+    if Settings.readAttribute("spider", False):
         urlFuzzer.spider()
 
-    if Settings.readAttribute("fuzz",False):
+    if Settings.readAttribute("fuzz", False):
         urlFuzzer.fuzz()
 
     shutdown()
